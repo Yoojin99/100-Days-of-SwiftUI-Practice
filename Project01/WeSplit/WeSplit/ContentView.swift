e4a1226eab19ff5df1267b45432933636bb34e4f
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-22 16:05:36 +0900

    Add comments for basic description

--- Project01/WeSplit/WeSplit/ContentView.swift
+++ Project01/WeSplit/WeSplit/ContentView.swift
@@ -4,10 +4,13 @@
 //
 //  Created by NHN on 2022/11/22.
 //
+// Contians the initial user interface (UI).
 
 import SwiftUI
 
+/// - description: SwiftUI의 View protocol 채택. View protocol은 화면에 무언가를 그리기 위해 꼭 채택해야 하는 기본 프로토콜이다. UIKit의 UIView와 같은 역할인듯.
 struct ContentView: View {
+    /// View protocol의 유일한 requirement
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -15,10 +18,16 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
-        .padding()
+        .padding() // modifier. 기본 메서드지만, 기존 데이터 + 새로 수정된 내용을 포함한 새로운 뷰를 리턴한다. 따라서 여기에서는 padding이 적용된 Image, Text 뷰가 리턴되었다.
     }
+    
+    // body 외에도 다른 프로퍼티, 메서드를 추가할 수 있다.
 }
 
+// 코드와 함께 UI 디자인의 미리보기를 보여준다.
+// 오른쪽에 preview가 보여지는 곳을 canvas라 한다. Canvas는 자동으로 기기 중 하나를 선택하는데, Xcode window의 기기에서 원하는 기기를 선택해서 지정할 수 있다.
+// 앱스토어에 업로드되는 최종 앱을 구성하지는 않고, Xcode에서만 사용할 수 있음
+// Canvas의 preview가 자주 멈추는데, 이럴 때 Option + Cmd + P를 눌러서 다시 재개할 수 있다.
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Project01/WeSplit/WeSplit/WeSplitApp.swift
+++ Project01/WeSplit/WeSplit/WeSplitApp.swift
@@ -4,6 +4,8 @@
 //
 //  Created by NHN on 2022/11/22.
 //
+// Contains code for launching your app.
+// If you create something when the app launches and keep it alive the entire time, put here.
 
 import SwiftUI
 
