cc5e35b8cd212c1ed3b4720df4a5c7203e9ef3d8
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-22 16:37:44 +0900

    Add navigation bar to contentview form

--- Project01/WeSplit/WeSplit/ContentView.swift
+++ Project01/WeSplit/WeSplit/ContentView.swift
@@ -12,15 +12,13 @@ import SwiftUI
 struct ContentView: View {
     /// View protocol의 유일한 requirement
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
             }
-            
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
+            .navigationTitle("SwiftUI")
         }
     }
     
