c4a1dde93ee0658b17c19e784ed9c2cacba155e6
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-22 15:26:04 +0900

    Create initial WeSplit project

---
+++ Project01/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by NHN on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
