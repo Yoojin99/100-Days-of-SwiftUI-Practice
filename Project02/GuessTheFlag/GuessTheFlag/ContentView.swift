c7261f548a855bcff4de2217865cc972348ac680
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-28 14:35:52 +0900

    Create VStack

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
-        Text("Hello, world")
-        Text("This is another text view")
+        VStack {
+            Text("Hello, world")
+            Text("This is another text view")
+        }
     }
 }
 
