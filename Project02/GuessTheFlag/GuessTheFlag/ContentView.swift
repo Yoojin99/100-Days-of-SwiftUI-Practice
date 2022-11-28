ab232b188d1222a2082008af692ea74d431bf778
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-29 08:51:40 +0900

    Add LinearGradient

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,18 +10,13 @@ import SwiftUI
 struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            
-            Text("Your content")
-                .foregroundColor(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        LinearGradient(
+            gradient: Gradient(stops: [
+                .init(color: .white, location: 0.45),
+                .init(color: .black, location: 0.55)
+            ]),
+            startPoint: .top,
+            endPoint: .bottom)
     }
 }
 
