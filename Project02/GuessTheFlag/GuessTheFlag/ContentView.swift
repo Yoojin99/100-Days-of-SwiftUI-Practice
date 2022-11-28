0ca617bb9960ca8d57cbdec15933a6f0f3ad1cea
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-29 08:57:43 +0900

    Create RadialGradient

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,13 +10,7 @@ import SwiftUI
 struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
-        LinearGradient(
-            gradient: Gradient(stops: [
-                .init(color: .white, location: 0.45),
-                .init(color: .black, location: 0.55)
-            ]),
-            startPoint: .top,
-            endPoint: .bottom)
+        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
     }
 }
 
