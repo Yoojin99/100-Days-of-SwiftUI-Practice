ad41b48a3aa81ba69732f4ae9fdf51ce842c4ffa
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-28 14:52:52 +0900

    Add more spacers

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,9 +11,11 @@ struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
         VStack {
+            Spacer()
             Text("First")
             Text("Second")
             Text("Third")
+            Spacer()
             Spacer()
         }
     }
