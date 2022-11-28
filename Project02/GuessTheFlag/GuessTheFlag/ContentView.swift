b775acba869d0630d56caf09a87ad93f58010a8f
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-28 15:11:36 +0900

    Color background red using background modifier

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,30 +10,10 @@ import SwiftUI
 struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
-        VStack {
-            HStack {
-                Text("Hi")
-                Text("Hi")
-                Text("Hi")
-            }
-            .border(Color.green, width: 1)
-            
-            HStack {
-                Text("Hi")
-                Text("Hi")
-                Text("Hi")
-            }
-            .border(Color.green, width: 1)
-            
-            HStack {
-                Text("Hi")
-                Text("Hi")
-                Text("Hi")
-            }
-            .border(Color.green, width: 1)
-
+        ZStack {
+            Text("Your content")
         }
-        .border(Color.green, width: 1)
+        .background(.red)
     }
 }
 
