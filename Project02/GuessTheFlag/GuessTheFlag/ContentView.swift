35827e24d6e16e81dcc0ac114f49267e75a3b68a
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-30 08:58:42 +0900

    Replace LinearGradient with RadialGradient

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -17,8 +17,11 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             
             // 바깥의 VStack은 spacing이 30있지만
             VStack(spacing: 30) {
