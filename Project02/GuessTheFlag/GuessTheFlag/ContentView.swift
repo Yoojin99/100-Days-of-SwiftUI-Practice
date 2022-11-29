a0f2dc512619991b245dbc7dac8e2bed10625fc7
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-29 09:18:20 +0900

    Create basic button

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        Button("Delete selection") {
+            executeDelete()
+        }
+    }
+    
+    func executeDelete() {
+        print("Now deleting...")
     }
 }
 
