a795bd7f931cc036f3a77798b8606b05d1d27a4f
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-29 09:34:07 +0900

    Add button has destructive role

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     // 화면에 출력할 수 있는 어떤 view를 받기를 기대한다. navigation view, form, text view 등등. View 프로토콜을 충족하기만 하면 됨
     var body: some View {
-        Button("Delete selection") {
-            executeDelete()
-        }
+        Button("Delete selection", role: ButtonRole.destructive, action: executeDelete)
     }
     
     func executeDelete() {
