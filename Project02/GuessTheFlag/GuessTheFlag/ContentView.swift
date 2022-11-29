1281df93a6403d2787647317ba05d656068b0d9b
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-11-29 16:05:03 +0900

    Create 3 flag buttons below the text in VStack

--- Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project02/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,9 +13,22 @@ struct ContentView: View {
     var correctAnswer = Int.random(in: 0...2)
 
     var body: some View {
-        VStack {
-            Text("Tap the flag of")
-            Text(countries[correctAnswer])
+        // 바깥의 VStack은 spacing이 30있지만
+        VStack(spacing: 30) {
+            // 내부의 VStack은 spacing이 없다.
+            VStack {
+                Text("Tap the flag of")
+                Text(countries[correctAnswer])
+            }
+            
+            ForEach(0..<3) { number in
+                Button {
+                    // flag was tapped
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                }
+            }
         }
     }
     
