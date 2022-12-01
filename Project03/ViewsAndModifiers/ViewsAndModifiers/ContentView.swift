b61f777f5ac73f94cb5c479e234acd59e1dcf681
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-12-01 14:50:20 +0900

    Create button colored red which size is 200x200

--- Project03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.red)
+        Button("Hello, world!") {
+            print(type(of: self.body))
+        }
+        .frame(width: 200, height: 200)
+        .background(.red)
     }
 }
 
