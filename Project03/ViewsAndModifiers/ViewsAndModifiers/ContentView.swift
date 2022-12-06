db0092808e9d5442b85e2cff9b584b9efced0c81
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-12-07 08:58:14 +0900

    Use environment modifier `font()`

--- Project03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Gryffindor")
+                .font(.largeTitle)
             Text("Hufflepuff")
             Text("Ravenclaw")
             Text("Slytherin")
