b7709990daf228ecef6c041b6351b7a10199c1da
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-12-07 11:03:42 +0900

    Create custom ViewModifier that makes large blue font

--- Project03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,32 +7,24 @@
 
 import SwiftUI
 
-// generics
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    // defines closure. Add ViewBuilder attribute for more flexibility
-    @ViewBuilder let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<columns, id: \.self) { column in
-                        content(row, column)
-                    }
-                }
-            }
-        }
+struct LargeBlue: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func largeBlue() -> some View {
+        modifier(LargeBlue())
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            Text("R\(row) C\(col)")
-        }
+        Text("Hi")
+            .largeBlue()
     }
 }
 
