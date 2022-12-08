1f20d671c529ef0c2f5f71e207d6903bad8b4209
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-12-08 15:52:38 +0900

    Create GameManger tests

---
+++ MileStone1_RockPaperScissors/MileStone1_RockPaperScissorsTests/GameManagerTests.swift
@@ -0,0 +1,19 @@
+//
+//  GameManagerTests.swift
+//  GameManagerTests
+//
+//  Created by NHN on 2022/12/08.
+//
+
+import XCTest
+
+final class GameManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}
